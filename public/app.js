ac32b0837dcb89f0dbed5fa0710f7848cb745ad7
Aleksandr Berezin <aberezin@logrocon.com>
2019-07-14 21:39:30 +0300

    добавил отправку/получение новых сообщений

--- public/app.js
+++ public/app.js
@@ -1,3 +1,43 @@
 const socket = io();
 
-new Vue({ el: "#app" });
+Vue.component("chat-message", {
+	props: ["message"],
+	template: `
+      <div class="message">
+          <div class="message-content z-depth-1">
+              {{message.text}}
+          </div>
+      </div>
+    `
+});
+
+new Vue({
+	el: "#app",
+	data: {
+		message: "",
+		messages: []
+	},
+	methods: {
+		sendMessage() {
+			const message = {
+				text: this.message
+			};
+
+			socket.emit("message:create", message, err => {
+				if (err) {
+					console.error(err);
+				} else {
+					this.message = "";
+				}
+			});
+		},
+		initConnection() {
+			socket.on("message:new", message => {
+				this.messages.push(message);
+			});
+		}
+	},
+	mounted() {
+		this.initConnection();
+	}
+});
--- server/server.js
+++ server/server.js
@@ -10,11 +10,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
+const message = (name, text) => {
+	return { name, text };
+};
+
 app.use(express.static(publicPath));
 
 server.listen(port, () => console.log(`Server has been start on port ${port}`));
 
 io.on("connection", socket => {
 	console.log("io connection");
-	socket.on("createMessage", data => console.log("createMessage", data));
+	socket.on("message:create", (data, callback) => {
+		if (!data) {
+			callback(`Message can't be empty`);
+		} else {
+			io.emit("message:new", message("Admin", data.text));
+			callback();
+		}
+	});
 });
