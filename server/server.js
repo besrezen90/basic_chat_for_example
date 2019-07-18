f3ac42439933b5517ab5ecb6f7d3df1752a5515b
Aleksandr <besrezen90@mail.ru>
2019-07-18 16:58:20 +0300

    правки

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const socket = require("socket.io");
 const http = require("http");
+const users = require("./users")();
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3030;
